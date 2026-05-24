fbc2c193aeda2ebd23e3f87ee072ee893e48601c
aadhikari2157-tech <a.adhikari.2157@westcliff.edu>
2026-05-24 19:37:21 +0545

    Update qno 1 assignment week 3 atm.py
    minor changes in spaces in elif and print

--- qno 1 assignment week 3 atm.py
+++ qno 1 assignment week 3 atm.py
@@ -7,11 +7,11 @@ daily_limit = 50000
 if amount % 500 != 0:
     print("Invalid amount. Must be a multiple of NPR 500.")
 elif amount > balance:
-     print("Insufficient balance.")
-     elif daily_withdrawn + amount > daily_limit:
+    print("Insufficient balance.")
+elif daily_withdrawn + amount > daily_limit:
     print("Daily withdrawal limit reached.")
 else:
     balance = balance - amount
     print("Withdrawal successful.")
- print("Your current balance after withdrawal: NPR", balance)
+    print("Your current balance after withdrawal: NPR", balance)
 
