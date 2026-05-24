c01caa3de9d8696b77df00f6ff0b29428e93a682
aadhikari2157-tech <a.adhikari.2157@westcliff.edu>
2026-05-24 19:50:59 +0545

    Update qno 2 online store discount.py

--- qno 2 online store discount.py
+++ qno 2 online store discount.py
@@ -1,4 +1,10 @@
 purchase = float(input("Enter total purchase amount (NPR): "))
 is_member = input("Are you a loyalty member? (yes/no): ")
 if purchase < 1000:
-    discount = 0
+    discount = 0    discount = 0
+elif purchase < 5000:
+    discount = 5
+elif purchase < 15000:
+    discount = 10
+else:
+    discount = 20
