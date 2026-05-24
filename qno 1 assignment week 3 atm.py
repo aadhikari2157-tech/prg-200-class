e69ff5021fb51903c161c17add026bfdd0d84210
aadhikari2157-tech <a.adhikari.2157@westcliff.edu>
2026-05-24 19:25:46 +0545

    Update qno 1 assignment week 3 atm.py

--- qno 1 assignment week 3 atm.py
+++ qno 1 assignment week 3 atm.py
@@ -1,7 +1,10 @@
-<<<<<<< HEAD
+<<<<<<< HEA
 balance = float(input("Enter your account balance (NPR): "))
 daily_withdrawn = float(input("Amount already withdrawn today (NPR): "))
 amount = float(input("Amount to withdraw (NPR): "))
 =======
 balance = float(input("Enter your account balance (NPR): "))
 >>>>>>> 0ed479b64e8e916cf7997a931b3d8275c9f6cc3d
+daily_limit = 50000
+
+if amount % 500 != 0:
