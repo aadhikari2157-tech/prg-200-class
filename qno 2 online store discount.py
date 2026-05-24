98c906dd34da361547faa9edc59f10620836745b
aadhikari2157-tech <a.adhikari.2157@westcliff.edu>
2026-05-24 19:57:47 +0545

    Update qno 2 online store discount.py

--- qno 2 online store discount.py
+++ qno 2 online store discount.py
@@ -10,7 +10,7 @@ else:
     discount = 20
 discounted_amount = purchase * (1 - discount / 100)
 if is_member.lower() == "yes" and discount > 0:
-     final_amount = discounted_amount * (1 - 5 / 100)
+    final_amount = discounted_amount * (1 - 5 / 100)
     print("Purchase Amount      : NPR", purchase)
     print("Purchase Discount    :", discount, "%")
     print("Loyalty Discount     : 5%")
