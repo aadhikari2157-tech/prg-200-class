5172ddf74265fe597f7dbd1b4f678760ba16bc85
aadhikari2157-tech <a.adhikari.2157@westcliff.edu>
2026-05-24 19:23:25 +0545

    Update qno 1 assignment week 3 atm.py

--- qno 1 assignment week 3 atm.py
+++ qno 1 assignment week 3 atm.py
@@ -1,2 +1,3 @@
-balance = float(input("Enter your account balance (NPR): "))daily_withdrawn = float(input("Amount already withdrawn today (NPR): "))
+balance = float(input("Enter your account balance (NPR): "))
+daily_withdrawn = float(input("Amount already withdrawn today (NPR): "))
 amount = float(input("Amount to withdraw (NPR): "))
