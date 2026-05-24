7691e12362b4f07e70161d20bae8f0935e129ee1
aadhikari2157-tech <a.adhikari.2157@westcliff.edu>
2026-05-24 20:47:25 +0545

    Update qno 4 password strength checker.py

--- qno 4 password strength checker.py
+++ qno 4 password strength checker.py
@@ -7,3 +7,14 @@ has_lower = False
 has_digit = False
 has_special = False
 special_characters = "!@#$%^&*"
+
+for char in password:
+     if char.isupper():
+        has_upper = True
+    if char.islower():
+        has_lower = True
+    if char.isdigit():
+        has_digit = True
+    if char in special_characters:
+        has_special = True
+        
