0997d68ee4f92401061f2a0c2edcb54a3e6cfcfb
aadhikari2157-tech <a.adhikari.2157@westcliff.edu>
2026-06-07 18:17:29 +0545

    Update week 5 qno 3 food delivery app.py

--- week 5 qno 3 food delivery app.py
+++ week 5 qno 3 food delivery app.py
@@ -1,2 +1,6 @@
 class DeliveryPartner:
-    
+
+    def __init__(self, name, partner_id, deliveries):
+        self.name = name
+        self.partner_id = partner_id
+        self.deliveries = deliveries
