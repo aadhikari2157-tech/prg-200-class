bc7ae390673a4c45cf7e2098ac436cd7758c9fce
aadhikari2157-tech <a.adhikari.2157@westcliff.edu>
2026-06-07 17:58:00 +0545

    Update week 5 qno 1 bank account manager.py

--- week 5 qno 1 bank account manager.py
+++ week 5 qno 1 bank account manager.py
@@ -1,2 +1,6 @@
 class BankAccount:
-    
+    def __init__(self, name, account_number, balance=0):
+        self.name = name
+        self.account_number = account_number
+        self.balance = balance
+        
